a1a087db91137b2dbb975835a9b3c8125ebeada5
Tyson Arnold <tarnol17@ut.utm.edu>
2025-04-22 16:28:37 +0000

    added to main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Christian McKenzie
+ * @author Tyson Arnold
+ * @date 2025-04-25
+ * @brief Main program for the Deque Toolkit
+ * 
+ * Interface for managing the deque toolkit.
+ */
 
+#include "deque.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello";
+}
